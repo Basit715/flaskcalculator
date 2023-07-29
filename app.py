471c3a1421b80c7a51ad2366bdaec35de3983984
Basit715 <basitabass27411@gmail.com>
2023-07-29 15:20:11 +0530

    calculator

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-from flask import Flask, request, render_template, jsonify
-import json
+from flask import Flask, request, render_template
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def welcome():
-     return "Welcome to new flask app"
+     return render_template('calculator.html')
 
-@app.route('/cal', methods = ["GET"])
+@app.route('/',methods=['POST'])    
 def math_operator():
-     operation = request.json["operation"]      # requesting from postman in the form of json
-     number1 = request.json["number1"]
-     number2 = request.json["number2"]
      
-     if operation == "add":
-          result = int(number1) + int(number2)
-     elif operation == "minus":
-          result = int(number1) - int(number2)
-     elif operation == "multiply":
-          result = int(number1)*int(number2)
-     elif operation == "division":
-          result = int(number1)/int(number2)
-     elif operation == "floor":
-          result = int(number1)//int(number2)
-     elif operation == "raised":
-          result = int(number1)**int(number2)
-     elif operation == "mod":
-          result = int(number1)%int(number2)
-     else:
-          print("operation not found")
+     # operation = request.json["operation"]      # requesting from postman in the form of json
+     # number1 = request.json["number1"]
+     # number2 = request.json["number2"]  
      
-     return jsonify(result)
+          operation = request.form.get('operation') 
+          n1 = request.form.get('number1', type=int, default=0)
+          n2 = request.form.get('number2', type=int, default=0)   
+          if operation == 'Addition':
+               result = n1 + n2  
+          elif operation == 'Subtraction':
+               result = n1 - n2
+          elif operation == 'Multiplication': 
+               result = n1 * n2
+          elif operation == 'Division': 
+               result = n1/n2
+          else:
+              result = 10
+               
+          entry = result      
+          return render_template('calculator.html', entry = entry)     
       
+            
+
 
 
 
 if __name__ == "__main__":
-     app.run(debug=True)
+     app.run(debug=True)  
